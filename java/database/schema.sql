1c656d05811148c116525d1be6bfbc462f673997
Caroline Lundy <carolinegracelundy@gmail.com>
2021-12-05 13:58:34 -0600

    Database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,12 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS event; 
+DROP TABLE IF EXISTS playlist; 
+DROP TABLE IF EXISTS songs; 
+DROP TABLE IF EXISTS genres; 
+DROP TABLE IF EXISTS dj; 
+DROP TABLE IF EXISTS hosts; 
+DROP TABLE IF EXISTS users; 
 DROP SEQUENCE IF EXISTS seq_user_id;
 
 CREATE SEQUENCE seq_user_id
@@ -16,7 +22,69 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
+); 
+
+CREATE TABLE hosts ( 
+        host_id int, 
+        host_name varchar(50) NOT NULL,
+        CONSTRAINT pk_host_id PRIMARY KEY (host_id),  
+        CONSTRAINT fk_host_id FOREIGN KEY (host_id) REFERENCES users(user_id) 
+        
+);   
+
+CREATE TABLE dj ( 
+        dj_id int, 
+        dj_name varchar(50) NOT NULL,
+        CONSTRAINT pk_dj_id PRIMARY KEY (dj_id),  
+        CONSTRAINT fk_dj_id FOREIGN KEY (dj_id) REFERENCES users(user_id) 
+        
+);     
+
+CREATE TABLE genres ( 
+        genre_id SERIAL, 
+        genre_name varchar(50) NOT NULL,
+        CONSTRAINT pk_genre_id PRIMARY KEY (genre_id)
+      
+        
+);    
+
+CREATE TABLE songs (  
+        song_id SERIAL, 
+        genre_id int, 
+        song_title varchar(50) NOT NULL, 
+        song_artist varchar(50) NOT NULL,
+        CONSTRAINT pk_song_id PRIMARY KEY (song_id),  
+        CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genres(genre_id)        
+);        
+
+CREATE TABLE playlist (  
+        playlist_id SERIAL, 
+        song_id int, 
+        playlist_name varchar(50) NOT NULL, 
+        CONSTRAINT pk_playlist_id PRIMARY KEY (playlist_id),  
+        CONSTRAINT fk_song_id FOREIGN KEY (song_id) REFERENCES songs(song_id)        
+);  
+
+CREATE TABLE event (  
+        event_id SERIAL, 
+        event_host int, 
+        event_dj int,
+        playlist_id int,  
+        event_name varchar(50) NOT NULL, 
+        event_date date, 
+        start_time time, 
+        duration_minutes int CHECK (duration_minutes >= 30),  
+        event_location varchar(50),
+        CONSTRAINT pk_event_id PRIMARY KEY (event_id),  
+        CONSTRAINT fk_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlist(playlist_id),   
+        CONSTRAINT fk_event_host FOREIGN KEY (event_host) REFERENCES hosts(host_id),
+        CONSTRAINT fk_event_dj FOREIGN KEY (event_dj) REFERENCES dj(dj_id)  
+);  
+
+
+
+
+
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
