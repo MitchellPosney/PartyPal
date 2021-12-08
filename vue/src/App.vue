4fbe4d52f9f3ae23b4a6f7561511ac73ca8795a6
Ashley Shewmaker <ashley.e.shewmaker@gmail.com>
2021-12-08 12:35:35 -0500

    added assets folder for imgs, started work on front end

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -7,3 +7,17 @@
     <router-view />
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  background-color: #000022;
+  color: aliceblue;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+
+</style>
