ac0b88289dee8bc48b5c5eb05b705e35e3a3a45a
Caroline Lundy <carolinegracelundy@gmail.com>
2021-12-09 19:51:08 -0600

    (Front End Services)

---
+++ vue/src/services/DjService.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+const http = axios.create({
+    baseURL: "http://localhost:3000"
+  });
+
+export default { 
+
+    createEvent(event) { 
+        return http.post('/event', event);
+    },
+
+addGenre(genre) { 
+    return http.post('/genres', genre);
+}, 
+
+deleteGenre(genreID) {
+    return http.delete(`/genres/${genreID}`);
+  }, 
+
+addSongToGenre(songID, genreID, song) { 
+ return http.post(`/addSongToGenre/${songID}/${genreID}`, song);
+}, 
+
+deleteSongFromGenre(song) { 
+return http.put(`/deleteSongFromGenre/${song.id}`, song);
+},
+
+addSongToPlaylist(songID, playlistID, song) { 
+  return http.post(`/addSongToPlaylist/${songID}/${playlistID}`, song);
+} 
+
+
+  
+
+}
