e4cf66f0cdbeab57159f4884aaff3badcba7aaa5
Mitchell Posney <mposney@gmail.com>
2021-12-10 12:24:19 -0500

    Pushing SHARED DAO

--- java/src/main/java/com/techelevator/dao/JdbcSharedDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcSharedDao.java
@@ -7,6 +7,7 @@ import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JdbcSharedDao implements SharedDao
@@ -21,7 +22,7 @@ public class JdbcSharedDao implements SharedDao
     public void getEventByName(String eventName)
     {
         String event = "SELECT * FROM event WHERE event_name = ?";
-        Event results = jdbcTemplate.queryForRowSet(event,eventName);
+        Event[] results = jdbcTemplate.queryForRowSet(event,eventName);
         mapRowToEvent();
     }
 
@@ -35,14 +36,34 @@ public class JdbcSharedDao implements SharedDao
         String event = "SELECT host_name FROM host WHERE host_id = ?";
         return jdbcTemplate.queryForObject(event, String.class, hostId);
     }
-    public String getPlaylist(long playlistId)
+    public List<Song> getPlaylist(long playlistId)
     {
-        String event = "SELECT host_name FROM host WHERE host_id = ?";
-        return jdbcTemplate.queryForRowSet(event, String.class, playlistId);
+        String event2 = "SELECT *, c.username AS sender_name, d.username AS receiver_name FROM song_playlist " +
+                "JOIN song_playlist AS a ON a.account_id = transfers.account_from " +
+                "JOIN accounts AS b ON b.account_id = transfers.account_to " +
+                "JOIN users AS c ON c.user_id = a.user_id " +
+                "JOIN users AS d ON d.user_id = b.user_id " +
+                "WHERE transfers.account_from = ? OR transfers.account_to = ?;";
+
+        String event = "SELECT * FROM song " +
+                        "JOIN song_playlist AS a ON a.account_id = transfers.account_from " +
+                        "WHERE playlist_id = ?";
+        List<Song> songs = new ArrayList<>();
+        SqlRowSet results = jdbcTemplate.queryForRowSet(event, String.class, playlistId);
+        while(results.next()) {
+            Song song = mapRowToSong(results);
+            songs.add(song);
+        }
+        return songs;
+    }
+
+    private Song mapRowToSong(SqlRowSet rs)
+    {
+        return new Song(rs.getLong("genre_id"),rs.getString("song_title"),rs.getString("song_artist"));
     }
 
     private Event mapRowToEvent(SqlRowSet rs)
     {
-        return = new Event(rs.getString("event_host"),rs.getString("event_dj"), (ArrayList<Song>) rs.getObject("event_id"),rs.getString("event_name"),rs.getDate("event_date"),rs.getTime("start_time"),rs.getInt("duration_minutes"),rs.getString("event_location"));
+        return new Event(rs.getString("event_host"),rs.getString("event_dj"), (ArrayList<Song>) rs.getObject("event_id"),rs.getString("event_name"),rs.getDate("event_date"),rs.getTime("start_time"),rs.getInt("duration_minutes"),rs.getString("event_location"));
     }
 }
